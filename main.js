c587af3cb1d32d9b6b59bcddeb000a1a7bbfa5e4
eduardooligini <145785071+eduardooligini@users.noreply.github.com>
2025-08-26 08:40:06 -0300

    26

--- main.js
+++ main.js
@@ -33,7 +33,7 @@ geraSenha();
 
 function geraSenha(){
     let senha = '';
-    for (let i=0; i<tamanhoSenha; i++){
+    for (let i=0; i<tamanhoSenha; i++) {
         let numeroAleatorio = Math.random()*letrasMaiusculas.length;
         numeroAleatorio = Math.floor(numeroAleatorio);
         senha = senha + letrasMaiusculas[numeroAleatorio]
